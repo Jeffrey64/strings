dc352d35d0b6c40ae65bcc1ff7b11fe7a6eb6eb3
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-09-14 11:19:28 -0700

    Initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,15 @@ public class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("Hello and salutations");
-        int i;
-        float f = 2.5;
+        int i = 10;
+        float f = 2.5f;
         boolean flag = true;
-        
+        f += i;
+        flag = !flag;
+        i += 1;
+        System.out.print(i++);
+        System.out.print(++f);
+    
     }
 }
 
